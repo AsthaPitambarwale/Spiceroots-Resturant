8e470e4c2ca0a11f680f8346a8dab10153c88230
AsthaPitambarwale <asthapitambarwale@gmail.com>
2026-06-13 23:26:21 +0530

    Intial

--- src/app/api/client.ts
+++ src/app/api/client.ts
@@ -1,4 +1,4 @@
-const API_URL = import.meta.env.VITE_API_URL || 'https://localhost:500/api';
+const API_URL = import.meta.env.VITE_API_URL;
 
 export interface ApiResponse<T = any> {
   success: boolean;
